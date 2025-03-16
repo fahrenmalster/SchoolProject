91742ceef2083ff813b89f1a8efd181792050727
fahrenmalster <fahrenmalster@gmail.com>
2025-03-16 11:39:52 +0000

    Add 10.php

---
+++ src/10.php
@@ -0,0 +1,13 @@
+<?php
+// Get the number of students in the class
+$numStudents = 25;
+
+// Generate a random student ID
+$studentID = rand(1, $numStudents);
+
+// Print the student's name and grade level
+echo "Student $studentID:";
+echo "<br>";
+echo "Name: ";
+echo "Grade: ";
+?>

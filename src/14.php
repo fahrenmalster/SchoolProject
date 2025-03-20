0b8105dbbb8678273639efaf2e593831e3259af5
fahrenmalster <fahrenmalster@gmail.com>
2025-03-20 19:23:18 +0000

    Add 14.php

---
+++ src/14.php
@@ -0,0 +1,9 @@
+<?php
+// This file contains some basic PHP code
+
+// Example: echo "Hello, World!";
+echo "Hello, World!";
+
+// Use variables in PHP like this: $myVar = 5; print "$myVar";
+$myVar = 10;
+echo "$myVar"; // Output: 10

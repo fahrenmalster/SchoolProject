7dd0127e0b1643f3f5b541a7b76aa185ab721395
fahrenmalster <fahrenmalster@gmail.com>
2025-04-11 12:10:40 +0000

    Add 30.php

---
+++ src/30.php
@@ -0,0 +1,12 @@
+<?php
+
+// This is just an example and not part of your school project code.
+// You need to replace the actual functions with what suits your needs.
+
+function helloWorld() {
+    echo "Hello, World!";
+}
+
+echo "\n";
+
+echo "This function will be called when the script starts.\n";
